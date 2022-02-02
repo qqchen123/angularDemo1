ed5993b54048392ae34e5c400114d826e15c73c4
chenenjie <785019257@qq.com>
2022-02-02 18:21:43 +0800

    js动态跳转路由

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
@@ -9,7 +9,7 @@ export class OrderComponent implements OnInit {
   public orderList: any = [];
   public msg: string = '我是子组件的msg';
 
-  constructor() {}
+  constructor(public router: Router) {}
 
   ngOnInit(): void {
     for (let i = 0; i < 10; i++) {
@@ -18,5 +18,12 @@ export class OrderComponent implements OnInit {
   }
   run() {
     alert('我是子组件的run方法');
+  }
+  jsUrl() {
+    // 适合普通路由和动态路由
+    this.router.navigate(['ordercontent/', 12323123]);
+  }
+  goToHome() {
+    this.router.navigate(['home']);
   }
 }
